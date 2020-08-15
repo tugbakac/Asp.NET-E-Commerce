9c95458ea786ecd80bba932f9e4288496d39b2be
tugbakac <tugbakac45@gmail.com>
2020-08-15 17:13:32 +0300

    first commit

---
+++ MusicStoreSites.UI.MVC/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using MusicStoreSites.BLL.Abstract;
+using MusicStoreSites.UI.MVC.CustomFilter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStoreSites.UI.MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        IAlbumService albumService;
+        public CartController(IAlbumService AlbumService)
+        {
+            albumService = AlbumService;
+        }
+        // GET: Cart
+
+        [CustomFilter]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _CartList()
+        {
+            return PartialView();
+        }
+    }
+}
